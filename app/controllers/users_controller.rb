8d937636772467ab64bbc935ea103f6c2eb77dd5
rastko <vucen.rastko@gmail.com>
2023-07-10 20:22:46 +0200

    Enable editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params) # see method below
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_url(@user), notice: 'User was successfully created.'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_url(@user), notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
